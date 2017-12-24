b66eac1ed78382b93d79340964b93b470e3eb1ef
Shane Orend <wagen00@gmail.com>
2017-12-23 19:02:04 -0500

    New location

---
+++ assets/Javascript/game.js
@@ -0,0 +1,61 @@
+window.onload = function(){
+
+ var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h",
+     "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+     "t", "u", "v", "w", "x", "y", "z"
+ ];
+ var Guesscar = ["Corvette ", "Golf ", "Miata ", "Supra "];
+ var win = getElementById(win) 
+ var lose = 0;
+ var guess = [];
+ var space;
+ var showLives = document.getElementById("Mleft");
+
+win = "win: " + 0;
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // var userGuess = event.key;
+//  document.onkeyup = function(event) {
+//  	var random = Math.floor((Math.random() * (Guesscar.length - 1)));
+//  var lsgwort = Guesscar[random];
+
+
+//  var ratewort = new Array(lsgwort.length);
+//  var fehler = 0;
+
+//  // every letter in the word is symbolized by an underscore in the guessfield
+//  for (var i = 0; i < ratewort.length; i++) {
+//      ratewort[i] = "_ ";
+//  };
+
+//  function printRatewort() {
+//      for (var i = 0; i < ratewort.length; i++) {
+//          var ratefeld = document.getElementById("MidMiddle");
+//          var buchstabe = document.createTextNode(ratewort[i]);
+//          ratefeld.appendChild(buchstabe);
+//      };
+//  };
+ 
+     
+
+//      };
+// };
+ 
+//  // for (var i = 0; i < Guesscar.length; i++) {
+//  //         Guesscar[i]
